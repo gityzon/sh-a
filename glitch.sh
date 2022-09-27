5b8df8117cb40214438b48b764e11896ead90c4f
gityzon <109445070+gityzon@users.noreply.github.com>
2022-09-27 23:23:49 +0800

    Update glitch.sh

--- glitch.sh
+++ glitch.sh
@@ -4,13 +4,48 @@ if [ ! -f "alist" ];then
   curl -L https://github.com/alist-org/alist/releases/latest/download/alist-linux-musl-amd64.tar.gz -o alist.tar.gz
   tar -zxvf alist.tar.gz
   rm -f alist.tar.gz
-  mv alist-linux-musl-amd64 alist
   chmod 777 alist
   
   #download config(listen on port 3000)
   mkdir data
   cd data
-  wget https://github.com/gityzon/sh-alist/raw/main/config.json
+  cat > config.json <<EOF
+{
+  "force": false,
+  "address": "0.0.0.0",
+  "port": 5244,
+  "site_url": "",
+  "cdn": "",
+  "jwt_secret": "sz01AkTpx0qqBDO7",
+  "token_expires_in": 48,
+  "database": {
+    "type": "sqlite3",
+    "host": "",
+    "port": 0,
+    "user": "",
+    "password": "",
+    "name": "",
+    "db_file": "data/data.db",
+    "table_prefix": "x_",
+    "ssl_mode": ""
+  },
+  "scheme": {
+    "https": false,
+    "cert_file": "",
+    "key_file": ""
+  },
+  "temp_dir": "data/temp",
+  "log": {
+    "enable": true,
+    "name": "log/log.log",
+    "max_size": 10,
+    "max_backups": 5,
+    "max_age": 28,
+    "compress": false
+  }
+}
+EOF
+
 fi
 #启动alist
-./alist -docker
+./alist server
