a867388353b331798773c343c7a1107d1a581676
gityzon <109445070+gityzon@users.noreply.github.com>
2022-12-28 15:51:27 +0800

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,51 @@
+
+if [ ! -f "alist" ];then
+  #download alist lastest
+  curl -L https://github.com/alist-org/alist/releases/latest/download/alist-linux-musl-amd64.tar.gz -o alist.tar.gz
+  tar -zxvf alist.tar.gz
+  rm -f alist.tar.gz
+  chmod 777 alist
+  
+  #download config(listen on port 3000)
+  mkdir data
+  cd data
+  cat > config.json <<EOF
+{
+  "force": false,
+  "address": "0.0.0.0",
+  "port": 3000,
+  "site_url": "",
+  "cdn": "",
+  "jwt_secret": "sz01AkTpx0qqBDO7",
+  "token_expires_in": 48,
+  "database": {
+    "type": "sqlite3",
+    "host": "",
+    "port": 0,
+    "user": "",
+    "password": "",
+    "name": "",
+    "db_file": "data/data.db",
+    "table_prefix": "x_",
+    "ssl_mode": ""
+  },
+  "scheme": {
+    "https": false,
+    "cert_file": "",
+    "key_file": ""
+  },
+  "temp_dir": "data/temp",
+  "log": {
+    "enable": true,
+    "name": "log/log.log",
+    "max_size": 10,
+    "max_backups": 5,
+    "max_age": 28,
+    "compress": false
+  }
+}
+EOF
+
+fi
+#启动alist
+./alist server
